04438311da45a60452654f60527b8fab7a7363b5
Rhaastrake <garofalomichele2001@gmail.com>
2026-09-26 13:21:41 +0200

    feat: move index and search into burger menu on mobile

--- src/frontend/js/pages/docs.js
+++ src/frontend/js/pages/docs.js
@@ -2,12 +2,23 @@ import '../global.js';
 
 import { initDocsNav } from "../modules/docsNav.js";
 import { initDocsVersion } from "../modules/docsVersion.js";
-import { initSearch, initSearchPlacement } from "../modules/search.js";
+import { initSearch } from "../modules/search.js";
+import { moveOnBreakpoint } from "../modules/responsiveMove.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     initDocsNav();
     initDocsVersion();
     initSearch();
-    initSearchPlacement();
+
+    moveOnBreakpoint(
+        document.querySelector(".site-search"),
+        document.getElementById("nav-search-slot"),
+        "(max-width: 430px)"
+    );
+    moveOnBreakpoint(
+        document.querySelector(".docs-index"),
+        document.getElementById("nav-index-slot"),
+        "(max-width: 991.98px)"
+    );
     // alert("Documentation is still incomplete, please be patient :D");
 })
